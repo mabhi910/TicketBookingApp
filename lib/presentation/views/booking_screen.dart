f0dbe2ada313d6435941c59ee7b06ca2dd3efcbd
Abhishek Mishra <abhishekmishra@singsys.com>
2023-07-31 15:12:52 +0530

    changes

--- lib/presentation/views/booking_screen.dart
+++ lib/presentation/views/booking_screen.dart
@@ -42,6 +42,7 @@ class _BookingScreenState extends State<BookingScreen> {
                   )),
                   child: Row(
                     children: [
+
                       Expanded(child: Text('Email: ${book[index]?.email}')),
                       const SizedBox(
                         width: 20,
